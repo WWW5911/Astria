832eadcf1354a3194cacc7e58ef44338538de2c0
WWW5911 <56112100+WWW5911@users.noreply.github.com>
2019-12-05 01:03:14 +0000

    Update backup.asm

--- backup.asm
+++ backup.asm
@@ -19,6 +19,7 @@ Finti1 BYTE "F(1) = 1", 0
 Finti0 BYTE "F(0) = 0", 0
 namee BYTE 16 dup(?)
 count DWORD 0
+ans DWORD 0
 .code
 main PROC
 	mov EDX, offset info
@@ -59,9 +60,21 @@ L2:
 	MOV ECX, 1
 	call fib
 
+	mov EDX, offset F1
+	call WriteString
+	mov EAX, count
+	call WriteDec
+	mov EDX, offset F5
+	call WriteString
+	mov eax, ans
+	call WriteDec
+	call Crlf
+
 main ENDP
 
 fib proc
+	CMP ECX, count
+	JE L5
 	push EBP
 	mov EBP, ESP ; STACK FRAMEEEE
 	sub ESP, 4 ;建立空間
@@ -86,31 +99,85 @@ fib proc
 	call Crlf
 	;印到這
 
-	cmp ecx, count
+	cmp ecx, count ;如果跑到尾巴
 	JE L3
 	call fib
+	cmp ECX, 2
+	JE L4
+
+	;開始正常狀態遞迴
+	push [EBP-28]
 	push [EBP-16]
-	;跑跑跑跑完了 開始往回 印
+	;印
 	mov EDX, offset F1
 	call WriteString
 	mov EAX, ECX
 	call WriteDec
 	mov EDX, offset F5
 	call WriteString
-	;印到這
-	pop EAX 
+	pop EAX ;F n-1 叫出來
 	call WriteDec
-	JMP L4
+	call Crlf
+	;印到這
+	mov DWORD PTR [EBP-4], 0
+	mov [EBP-16], EAX ;放回去
+	mov [EBP-4], EAX ;存起來
+	pop EAX
+	push [EBP-16]
+	push EAX
+	;印
+	mov EDX, offset F1
+	call WriteString
+	mov EAX, ECX
+	dec EAX
+	call WriteDec
+	mov EDX, offset F5
+	call WriteString
+	pop EAX ;F n-2 叫出來
+	call WriteDec
+	call Crlf
+	;印到這
+	add [EBP-4], EAX ;加起來
+	mov [EBP-28], EAX ;放回去
+	pop EAX
+	mov [EBP-16], EAX ;放回去
+	
+	inc ECX
+	JMP L5
 L3:
+	sub ESP, 4 ;建立空間
 	mov EDX, offset Finti1
 	call WriteString
 	call Crlf
 	mov EDX, offset Finti0
 	call WriteString
 	call Crlf
-	mov DWORD PTR [EBP-4], 10
+	mov DWORD PTR [EBP-4], 1
+	push 1
 	mov ECX, 2
+	JMP L5
 L4:
+	push [EBP-16]
+	mov EDX, offset F1
+	call WriteString
+	mov EAX, ECX
+	call WriteDec
+	mov EDX, offset F5
+	call WriteString
+	mov EAX, 1
+	call WriteDec
+	call Crlf
+	mov EDX, offset Finti1
+	call WriteString
+	call Crlf
+	;印到這
+	push EAX
+	mov [EBP-16], EAX
+	mov dword ptr [EBP-4], 2
+	inc ECX
+L5:
+	mov eax, Dword ptr [EBP-4]
+	mov ans, eax
 	mov esp, ebp
 	pop ebp
 	ret
