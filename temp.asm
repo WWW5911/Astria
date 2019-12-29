f5b59bb5618740d22b94dc471b64f21ae690ea04
WWW5911 <56112100+WWW5911@users.noreply.github.com>
2019-12-29 16:31:23 +0800

    Create temp.asm

---
+++ temp.asm
@@ -0,0 +1,10 @@
+map_v0 BYTE "                                                 ", 0
+map_v01 BYTE "                                                 ", 0
+map_v02 BYTE "                                                 ", 0
+map_v7 BYTE "                                  To forest |    ", 0
+map_v1 BYTE "                       __________           |    ", 0
+map_v2 BYTE "      -----            |Worker's|           |    ", 0
+map_v3 BYTE "     /_____\          /----------\          v    ", 0
+map_v4 BYTE "    /|     |\         |      _   |               ", 0
+map_v5 BYTE "     |     |          |     /.\  |          O    ", 0
+map_v6 BYTE "_____|_____|__________|_____|_|__|__________O__   ", 0
